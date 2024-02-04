74abcdf7c7700d8c41411f9464e80f4ff6b02bac
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-02-03 23:59:06 -0500

    refactor the displaying of the grid

--- eas.js
+++ eas.js
@@ -1,16 +1,27 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('#changeGrid');
 
-for(let s = 1; s <= 256; s++){
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.style.backgroundColor = 'white';
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
-    })
-    container.appendChild(square);
-}
 
 button.addEventListener('click', () => {
-    const newGrid = prompt('Must be between 16 - 100', 16);
+    let newGrid = 0;
+    while(newGrid < 16 || newGrid > 100){
+        newGrid = +prompt('Must be between 16 - 100', 16);
+    }
 })
+
+
+function displayGrid(grid = 256){
+
+    for(let s = 1; s <= grid; s++){
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.style.backgroundColor = 'white';
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
+        container.appendChild(square);
+    }
+
+}
+
+displayGrid();
