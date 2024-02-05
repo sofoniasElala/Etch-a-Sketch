ae80cde39ed5646998fdb3b027661404aa40a2e6
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-02-05 13:26:14 -0500

    randomize the colors
    the color each grid item changes to are now randomized.
    this commit also contains minor refactoring and styling changes

--- eas.js
+++ eas.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('#changeGrid');
-
+let currentGrid = 16;
 
 button.addEventListener('click', () => {
     let newGrid = 0;
     while(newGrid < 16 || newGrid > 100){
-        newGrid = +prompt('Must be between 16 - 100', 16);
+        newGrid = +prompt('Must be between 16 - 100', currentGrid);
     }
+    currentGrid = newGrid;
     displayGrid(500/newGrid, newGrid * newGrid);
 })
 
@@ -20,11 +21,15 @@ function displayGrid(size = 31.25,grid = 256){
         square.style.height = size + 'px';
         square.style.backgroundColor = 'white';
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'red';
+            square.style.backgroundColor = randomRgbValue();
         })
         container.appendChild(square);
     }
 
+}
+
+function randomRgbValue(){
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
 }
 
 displayGrid();
