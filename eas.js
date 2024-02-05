ab5676d70b870a63edbbfdaa07edaaf8fb681689
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-02-05 13:00:06 -0500

    allow user to change grid size

--- eas.js
+++ eas.js
@@ -7,14 +7,17 @@ button.addEventListener('click', () => {
     while(newGrid < 16 || newGrid > 100){
         newGrid = +prompt('Must be between 16 - 100', 16);
     }
+    displayGrid(500/newGrid, newGrid * newGrid);
 })
 
 
-function displayGrid(grid = 256){
-
+function displayGrid(size = 31.25,grid = 256){
+    container.textContent = '';
     for(let s = 1; s <= grid; s++){
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.style.width = size + 'px';
+        square.style.height = size + 'px';
         square.style.backgroundColor = 'white';
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'red';
